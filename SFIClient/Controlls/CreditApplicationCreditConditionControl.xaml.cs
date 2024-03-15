d2a99a0fc05cb3344cb093e305b67889046eb731
Angel Garcia <adjcg15@gmail.com>
2024-03-14 19:05:39 -0600

    feat(CreditApplicationCreditConditionControl): creates credit condition user control for credit application view

---
+++ SFIClient/Controlls/CreditApplicationCreditConditionControl.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SFIClient.Controlls
+{
+    /// <summary>
+    /// Lógica de interacción para CreditApplicationCreditConditionControl.xaml
+    /// </summary>
+    public partial class CreditApplicationCreditConditionControl : UserControl
+    {
+        public CreditApplicationCreditConditionControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
