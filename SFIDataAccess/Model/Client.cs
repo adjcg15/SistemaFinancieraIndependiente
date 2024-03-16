77fdf45a9759c33f32b2f2c3e1731ea8838ddd36
rodrigo19112003 <zS21013854@estudiantes.uv.mx>
2024-03-16 17:11:10 -0600

    feature(Model: Client): implements Client model

---
+++ SFIDataAccess/Model/Client.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SFIDataAccess.Model
+{
+    [DataContract]
+    public class Client
+    {
+        [DataMember]
+        public DateTime Birthdate { get; set; }
+
+        [DataMember]
+        public string Curp { get; set; }
+
+        [DataMember]
+        public string LastName { get; set; }
+
+        [DataMember]
+        public string Name { get; set; }
+
+        [DataMember]
+        public string Rfc { get; set; }
+
+        [DataMember]
+        public string Surname { get; set; }
+
+        [DataMember]
+        public int Id_address { get; set; }
+
+        [DataMember]
+        public int Id_work_center { get; set; }
+
+        [DataMember]
+        public string Card_number { get; set; }
+    }
+}
