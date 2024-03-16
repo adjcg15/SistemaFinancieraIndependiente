bcafc38c483218b84f0ba5b27d36891f4de7f420
rodrigo19112003 <zS21013854@estudiantes.uv.mx>
2024-03-16 17:20:55 -0600

    feature(service): implements interface and client service class

---
+++ SFIServices/Services/ClientsService.cs
@@ -0,0 +1,18 @@
+﻿using SFIDataAccess.Model;
+using SFIServices.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SFIServices.Services
+{
+    public partial class SFIService : IClientsService
+    {
+        public Client RecoverClients()
+        {
+            
+        }
+    }
+}
