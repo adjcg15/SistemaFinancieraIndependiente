a3f72343a774b0617358e4c6eaa91262398b455a
rodrigo19112003 <zS21013854@estudiantes.uv.mx>
2024-03-17 11:06:00 -0600

    feature(ModifyBankAccount): implements view and verification field with incorrect format

---
+++ SFIClient/Views/ModifyBankAccountView.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SFIClient.Views
+{
+    /// <summary>
+    /// Lógica de interacción para ModifyBankAccountView.xaml
+    /// </summary>
+    public partial class ModifyBankAccountView : Page
+    {
+        public ModifyBankAccountView()
+        {
+            InitializeComponent();
+        }
+
+        private void TbCardNumber_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox tbCardNumber = sender as TextBox;
+
+            Style textInputStyle = (Style)this.FindResource("TextInput");
+            Style textInputErrorStyle = (Style)this.FindResource("TextInputError");
+
+            string cardNumber = tbCardNumber.Text.Trim();
+
+            if (cardNumber.Length != 16)
+            {
+                tbCardNumber.Style = textInputErrorStyle;
+            }
+            else
+            {
+                tbCardNumber.Style = textInputStyle;
+            }
+        }
+    }
+}
