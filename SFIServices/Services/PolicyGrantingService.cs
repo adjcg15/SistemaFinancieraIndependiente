74f38ef0a8eff5ed262c8fad6472efb7288791c8
DonajiNavarro15 <ada_hack49@hackademy.mx>
2024-03-18 18:17:14 -0600

    fix(RegisterPolicyGrantingCredit):incorporation of the views and logic corresponding to the registration of the granting policy in this branch that has the registration of the condition

---
+++ SFIServices/Services/PolicyGrantingService.cs
@@ -0,0 +1,19 @@
+﻿using SFIDataAccess.DataAccessObjects;
+using SFIDataAccess.Model;
+using SFIServices.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SFIServices
+{
+    public partial class SFIService : IPolicyService
+    {
+        public bool RegisterPolicyGranting(PolicyGranting NewPolicy)
+        {
+            return PolicyGrantingDAO.RegisterPolicyGranting(NewPolicy);
+        }
+    }
+}
