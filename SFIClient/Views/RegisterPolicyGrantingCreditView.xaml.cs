f389161d4ed5ad47c80ba6a0d186852efcb8ad5f
DonajiNavarro15 <ada_hack49@hackademy.mx>
2024-03-19 19:36:57 -0600

    fature(RegisterPolicyGrantingCredit): logic for the screen and make the components work

--- SFIClient/Views/RegisterPolicyGrantingCreditView.xaml.cs
+++ SFIClient/Views/RegisterPolicyGrantingCreditView.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,9 +21,202 @@ namespace SFIClient.Views
     /// </summary>
     public partial class RegisterPolicyGrantingCreditView : Page
     {
+        private readonly PolicyGranting newPolicy = new PolicyGranting();
         public RegisterPolicyGrantingCreditView()
         {
             InitializeComponent();
+            DpkEffectiveDate.DisplayDateStart = new DateTime(DateTime.Now.Year, DateTime.Now.Month, 1);
+        }
+        private bool VerifyPolicyInformation()
+        {
+            bool isValidPolicy = true;
+
+            if (string.IsNullOrEmpty(TbPolicyName.Text))
+            {
+                HighlightInvalidFields(TbPolicyName);
+                isValidPolicy = false;
+            }
+            else
+            {
+                ClearFieldHighlight(TbPolicyName);
+            }
+
+            if (!RbActivePolicy.IsChecked.HasValue || !RbInactivePolicy.IsChecked.HasValue ||
+                (!RbActivePolicy.IsChecked.Value && !RbInactivePolicy.IsChecked.Value))
+            {
+                HighlightInvalidFields(RbActivePolicy);
+                HighlightInvalidFields(RbInactivePolicy);
+                isValidPolicy = false;
+            }
+            else
+            {
+                ClearFieldHighlight(RbActivePolicy);
+                ClearFieldHighlight(RbInactivePolicy);
+            }
+
+            if (!DpkEffectiveDate.SelectedDate.HasValue)
+            {
+                HighlightInvalidFields(DpkEffectiveDate);
+                isValidPolicy = false;
+            }
+            else
+            {
+                ClearFieldHighlight(DpkEffectiveDate);
+            }
+
+            if (string.IsNullOrEmpty(TbPolicyDescription.Text))
+            {
+                HighlightInvalidFields(TbPolicyDescription);
+                isValidPolicy = false;
+            }
+            else
+            {
+                ClearFieldHighlight(TbPolicyDescription);
+            }
+
+            return isValidPolicy;
+        }
+        private void HighlightInvalidFields(Control control)
+        {
+            Style errorStyle = null;
+
+            if (control is TextBox)
+            {
+                errorStyle = (Style)FindResource("TextInputError");
+            }
+            else if (control is ComboBox)
+            {
+                errorStyle = (Style)FindResource("ComboBoxError");
+            }
+
+            else if (control is RadioButton)
+            {
+                errorStyle = (Style)FindResource("RadioButtonError");
+                control.Style = errorStyle;
+            }
+
+            if (errorStyle != null)
+            {
+                control.Style = errorStyle;
+            }
+        }
+        private void ClearFieldHighlight(Control control)
+        {
+            Style defaultStyle = null;
+
+            if (control is TextBox)
+            {
+                defaultStyle = (Style)FindResource("TextInput");
+            }
+            else if (control is ComboBox)
+            {
+                defaultStyle = (Style)FindResource("ComboBox");
+            }
+            else if (control is RadioButton)
+            {
+                defaultStyle = (Style)FindResource("RadioButtonStyle");
+                control.Style = defaultStyle;
+            }
+            if (defaultStyle != null)
+            {
+                control.Style = defaultStyle;
+            }
+        }
+        private void BtnSavePolicyGrantingCreditClick(object sender, RoutedEventArgs e)
+        {
+            bool isValidPolicy = VerifyPolicyInformation();
+            if (isValidPolicy)
+            {
+                newPolicy.Title = TbPolicyName.Text.Trim();
+                newPolicy.Description = TbPolicyDescription.Text.Trim();
+                newPolicy.EffectiveDate = DpkEffectiveDate.SelectedDate ?? DateTime.Now;
+                newPolicy.IsActive = RbActivePolicy.IsChecked ?? false;
+
+                ShowCreateNewPolicyConfirmDialog();
+            }
+            else
+            {
+                ShowInvalidFieldsDialog();
+            }
+        }
+        private void ShowInvalidFieldsDialog()
+        {
+            MessageBox.Show("Por favor, verifique que los campos marcados en rojo cuenten con una respuesta y que haya seleccionado una fecha de vigencia.",
+                                "Campos requeridos", MessageBoxButton.OK, MessageBoxImage.Warning);
+        }
+        private void ShowCreateNewPolicyConfirmDialog()
+        {
+            MessageBoxResult buttonClicked = MessageBox.Show(
+                $"¿Desea registrar la política de otorgamiento de crédito {newPolicy.Title}?",
+                "Confirme el registro de la solicitud",
+                MessageBoxButton.YesNo,
+                MessageBoxImage.Question
+            );
+
+            if (buttonClicked == MessageBoxResult.Yes)
+            {
+                RegisterPolicyGranting();
+            }
+
+        }
+        private void RegisterPolicyGranting()
+        {
+            PolicyServiceClient policyClient = new PolicyServiceClient();
+            try
+            {
+                bool isRegistered = policyClient.RegisterPolicyGranting(newPolicy);
+                if (isRegistered)
+                {
+                    MessageBox.Show($"La política de otorgamiento de crédito {newPolicy.Title} se ha registrado correctamente.",
+                                    "Registro exitoso", MessageBoxButton.OK, MessageBoxImage.Information);
+                    ClearPolicyFields();
+                }
+                else
+                {
+                    MessageBox.Show($"Ya existe una política de otorgamiento de crédito con el nombre {newPolicy.Title}. No se pudo registrar.",
+                                    "Error de registro", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+            catch (System.ServiceModel.FaultException<ServiceFault> fault)
+            {
+                ShowErrorRecoveringCreditTypesDialog(fault.Detail.Message);
+            }
+            catch (System.ServiceModel.EndpointNotFoundException)
+            {
+                string errorMessage = "El servidor no se encuentra disponible, intente más tarde";
+                ShowErrorRecoveringCreditTypesDialog(errorMessage);
+            }
+            catch (CommunicationException)
+            {
+                string errorMessage = "No fue posible acceder a la información debido a un error de conexión";
+                ShowErrorRecoveringCreditTypesDialog(errorMessage);
+            }
+        }
+        private void ShowErrorRecoveringCreditTypesDialog(string message)
+        {
+            MessageBoxResult buttonClicked = MessageBox.Show(
+                message,
+                "No fue posible recuperar los tipos de crédito existentes, por favor intenta más tarde",
+                MessageBoxButton.OK,
+                MessageBoxImage.Error
+            );
+            if (buttonClicked == MessageBoxResult.OK)
+            {
+                RedirectToMainMenu();
+            }
+        }
+        private void RedirectToMainMenu()
+        {
+            //TODO: redireccionar cuando exista el menú principal
+            Console.WriteLine("Redireccionando al menú principal...");
+        }
+        private void ClearPolicyFields()
+        {
+            TbPolicyName.Text = string.Empty;
+            TbPolicyDescription.Text = string.Empty;
+            DpkEffectiveDate.SelectedDate = null;
+            RbActivePolicy.IsChecked = false;
+            RbInactivePolicy.IsChecked = false;
         }
     }
 }
