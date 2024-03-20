e889a7bc9e94860296874dc1ba2d357da8a04971
Angel Garcia <adjcg15@gmail.com>
2024-03-19 21:27:43 -0600

    feat(GrantingPolicyControl): creates user control to show credit granting policies and handles event to change its layout dynamically

--- SFIClient/Views/CreditGrantingPolicyListView.xaml.cs
+++ SFIClient/Views/CreditGrantingPolicyListView.xaml.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using SFIClient.Controlls;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -20,6 +22,34 @@ namespace SFIClient.Views
         public CreditGrantingPolicyListController()
         {
             InitializeComponent();
+
+            SizeChanged += CreditGrantingPolicyListViewSizeChanged;
+        }
+
+        private void CreditGrantingPolicyListViewSizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            Page page = (Page)sender;
+            double pageWidth = page.ActualWidth;
+
+            ItemsPanelTemplate grantingPoliciesPanelTemplate = new ItemsPanelTemplate(new FrameworkElementFactory(typeof(UniformGrid)));
+            int totalPanelColumns = 1;
+            if(pageWidth > 800)
+            {
+                totalPanelColumns = 2;
+            }
+            
+            if(pageWidth > 1200)
+            {
+                totalPanelColumns = 3;
+            }
+
+            if (pageWidth > 1700)
+            {
+                totalPanelColumns = 4;
+            }
+
+            grantingPoliciesPanelTemplate.VisualTree.SetValue(UniformGrid.ColumnsProperty, totalPanelColumns);
+            ItcGrantingPolicies.ItemsPanel = grantingPoliciesPanelTemplate;
         }
     }
 }
