c292ab8027bc6f0aa2aac2deed69c747ffb40752
rodrigo19112003 <zS21013854@estudiantes.uv.mx>
2024-04-20 17:26:26 -0600

    feature(GUI): implementation GUI to Consult personal information of Client

---
+++ SFIClient/Views/ClientInformationView.xaml.cs
@@ -0,0 +1,61 @@
+﻿using SFIClient.SFIServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SFIClient.Views
+{
+    /// <summary>
+    /// Lógica de interacción para ClientInformationView.xaml
+    /// </summary>
+    public partial class ClientInformationController : Page
+    {
+        private readonly Client client;
+        public ClientInformationController(Client client)
+        {
+            InitializeComponent();
+            this.client = client;
+        }
+
+        private void PageLoaded(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void LoadClient()
+        {
+
+        }
+
+        private void BtnGoToSearchClientByRFCViewClick(object sender, RoutedEventArgs e)
+        {
+            RedirectToSearchClientByRFCView();
+        }
+
+        private void RedirectToSearchClientByRFCView()
+        {
+            NavigationService.Navigate(new SearchClientByRFCController());
+        }
+
+        private void ShowErrorRecoveringClient(string message)
+        {
+            MessageBox.Show(
+                message,
+                "Sistema no disponible",
+                MessageBoxButton.OK,
+                MessageBoxImage.Error
+            );
+        }
+    }
+}
