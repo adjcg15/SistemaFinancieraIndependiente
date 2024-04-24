bc38f5917983ac1a5a0834c289f6f43548269c80
Angel Garcia <adjcg15@gmail.com>
2024-04-24 11:12:21 -0600

    feat(data access): implements logic to recover credit applications list in creditsDAO and creates Dictum entity in model folder. Also updates SFIDatabaseModel to take into account not null in dictums table columns

---
+++ SFIDataAccess/Model/Dictum.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SFIDataAccess.Model
+{
+    [DataContract]
+    public class Dictum
+    {
+        [DataMember]
+        public DateTime GenerationDate { get; set; }
+
+        [DataMember]
+        public bool IsApproved { get; set; }
+
+        [DataMember]
+        public string Justification { get; set; }
+    }
+}
