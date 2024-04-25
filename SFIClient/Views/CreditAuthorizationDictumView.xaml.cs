51abd83e0443a7faf928d9856b8dc1dbf2a48cd3
rodrigo19112003 <131178299+rodrigo19112003@users.noreply.github.com>
2024-04-24 18:47:28 -0600

    pr(development): implementation recover credit granting polices to generate dictum (#24)
    * feat(polices): implementation getting credit granting polices
    
    * fix(login): change from home page to login

--- SFIClient/Views/CreditAuthorizationDictumView.xaml.cs
+++ SFIClient/Views/CreditAuthorizationDictumView.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using SFIClient.SFIServices;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,12 +22,102 @@ namespace SFIClient.Views
     /// </summary>
     public partial class CreditAuthorizationDictumController : Page
     {
+        private readonly CreditGrantingPoliciesClient creditGrantingPoliciesClient = new CreditGrantingPoliciesClient();
+        private CreditGrantingPolicy[] creditGrantingPolicesList;
         public CreditAuthorizationDictumController()
         {
             InitializeComponent();
         }
 
         private void PageLoaded(object sender, RoutedEventArgs e)
+        {
+            LoadCreditGrantingPolices();
+        }
+
+        private void LoadCreditGrantingPolices()
+        {
+            try
+            {
+                creditGrantingPolicesList = creditGrantingPoliciesClient.GetAllCreditGrantingPolicies();
+                if (creditGrantingPolicesList.Length != 0)
+                {
+                    LoadCreditApplicationContent("1234567890");
+                }
+                else
+                {
+                    ShowNoPolicesExistsAlertDialog();
+                    RedirectToCreditApplicationsListView();
+                }
+            }
+            catch (FaultException<ServiceFault> fe)
+            {
+                ShowErrorRecoveringCreditGrantingPolices(fe.Message);
+            }
+            catch (EndpointNotFoundException)
+            {
+                string errorMessage = "Por el momento el servidor no se encuentra disponible, intente más tarde";
+                ShowErrorRecoveringCreditGrantingPolices(errorMessage);
+                RedirectToCreditApplicationsListView();
+            }
+            catch (CommunicationException)
+            {
+                string errorMessage = "Por el momento el servidor no se encuentra disponible, intente más tarde";
+                ShowErrorRecoveringCreditGrantingPolices(errorMessage);
+                RedirectToCreditApplicationsListView();
+            }
+        }
+
+        private void ShowNoPolicesExistsAlertDialog()
+        {
+            MessageBoxResult messageBoxResult = MessageBox.Show(
+                "Por el momento no se puede generar ningún dictamen",
+                "No existen políticas registradas",
+                MessageBoxButton.OK,
+                MessageBoxImage.Error
+            );
+        }
+
+        private void ShowErrorRecoveringCreditGrantingPolices(string message)
+        {
+
+        }
+
+        private void RedirectToCreditApplicationsListView()
+        {
+            NavigationService.Navigate(new SearchClientByRFCController());
+        }
+
+        private void LoadCreditApplicationContent(string invoice)
+        {
+
+        }
+
+        private void ShowErrorRecoveringCreditApplicationContent(string message)
+        {
+
+        }
+
+        private void ShowCreditApplicationContent(CreditApplication creditApplication)
+        {
+
+        }
+
+        private void BtnDownloadINEClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnDownloadProofOfAddressSClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnDownloadProofOfIncomeClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SaveDocumentToPath()
         {
 
         }
