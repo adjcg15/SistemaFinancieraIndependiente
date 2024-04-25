8dc0c9d1a9065bbc021fad3fdc52dc069c7d719f
Ángel de Jesús de la Cruz García <82688573+adjcg15@users.noreply.github.com>
2024-04-25 13:03:21 -0600

    pr(development): modify credit granting policy view
    * feat(ModifyCreditGrantingPolicyView): creates view and implements navigation bar
    
    * feat(ModifyCreditGrantingPolicyView): implements gui structure for form and buttons
    
    * fix(ModifyCreditGrantingPolicyView): changes controller name

---
+++ SFIClient/Views/ModifyCreditGrantingPolicyView.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SFIClient.Views
+{
+    public partial class ModifyCreditGrantingPolicyController : Page
+    {
+        public ModifyCreditGrantingPolicyController()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnReturnToPreviousPageClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnCancelClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnSaveChangesClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
