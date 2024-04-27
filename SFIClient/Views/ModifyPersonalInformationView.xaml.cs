8f2e3a9a65b650e446efcb101438153c2df24e26
Ángel de Jesús de la Cruz García <82688573+adjcg15@users.noreply.github.com>
2024-04-27 12:16:05 -0600

    pr(development): update client personal information gui main logic
    * feat(ModifyPersonalInformationView): implements logic to redirect to previous page
    
    * feat(ModifyPersonalInformationView): implements logic to discard changes
    
    * feat(ModifyPersonalInformationView): applies restrictions to information that can be introduced in fields and generates methods to validate fields when saving changes
    
    * feat(ModifyPersonalInformationView): implements method to highlight all invalid fields
    
    * feat(ModifyPersonalInformationView): implements alert dialog for invalid fields
    
    * feat(ModifyPersonalInformationView): implements confirmation dialog when updating client information
    
    * feat(ModifyPersonalInformationView): fixes bug with client age validation

--- SFIClient/Views/ModifyPersonalInformationView.xaml.cs
+++ SFIClient/Views/ModifyPersonalInformationView.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using SFIClient.SFIServices;
+using SFIClient.Utilities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,6 +21,7 @@ namespace SFIClient.Views
     public partial class ModifyPersonalInformationController : Page
     {
         private readonly string clientRFC;
+        private Client client;
 
         public ModifyPersonalInformationController(string clientRFC)
         {
@@ -30,6 +32,7 @@ namespace SFIClient.Views
         private void PageLoaded(object sender, RoutedEventArgs e)
         {
             LoadClientPersonalInformation();
+            ApplyFieldsRestrictions();
         }
 
         private void LoadClientPersonalInformation()
@@ -39,6 +42,7 @@ namespace SFIClient.Views
             try
             {
                 Client client = clientsService.GetClientPersonalInformation(clientRFC);
+                this.client = client;
                 ShowClientPersonalInformation(client);
             }
             catch (FaultException<ServiceFault> fault)
@@ -57,6 +61,22 @@ namespace SFIClient.Views
                     "su conexión de Internet e intente de nuevo";
                 ShowErrorRecoveringClientPersonalInformationDialog(errorMessage);
             }
+        }
+
+        private void ApplyFieldsRestrictions()
+        {
+            RestrictToPlainText(TbClientName);
+            RestrictToPlainText(TbClientLastName);
+            RestrictToPlainText(TbClientSurname);
+            RestrictToPlainText(TbAddressStreet);
+            RestrictToPlainText(TbAddressNeighborhood);
+            RestrictToPlainText(TbAddressCity);
+            RestrictToPlainText(TbAddressMunicipality);
+            RestrictToPlainText(TbAddressState);
+
+            RestrictToNumeric(TbAddressInteriorNumber);
+            RestrictToNumeric(TbAddressOutdoorNumber);
+            RestrictToNumeric(TbAddressPostCode);
         }
 
         private void ShowErrorRecoveringClientPersonalInformationDialog(string message)
@@ -105,17 +125,227 @@ namespace SFIClient.Views
 
         private void BtnReturnToPreviousPageClick(object sender, RoutedEventArgs e)
         {
+            ShowReturnToPreviousPageConfirmationDialog();
+        }
 
+        private void ShowReturnToPreviousPageConfirmationDialog()
+        {
+            MessageBoxResult buttonClicked = MessageBox.Show(
+                "¿Está seguro que desea regresar a la ventana previa? Todos los cambios sin guardar se perderán",
+                "Regresar a ventana previa",
+                MessageBoxButton.YesNo,
+                MessageBoxImage.Question
+            );
+
+            if (buttonClicked == MessageBoxResult.Yes)
+            {
+                RedirectToSearchClientByRFC();
+            }
+        }
+
+        private void RedirectToSearchClientByRFC()
+        {
+            NavigationService.Navigate(new SearchClientByRFCController());
         }
 
         private void BtnCancelClick(object sender, RoutedEventArgs e)
         {
+            ShowDiscardChangesConfirmationDialog();
+        }
 
+        private void ShowDiscardChangesConfirmationDialog()
+        {
+            MessageBoxResult buttonClicked = MessageBox.Show(
+                "¿Está seguro que desea cancelar la actualización del cliente? Todos los cambios " +
+                "sin guardar se perderán",
+                "Descartar cambios",
+                MessageBoxButton.YesNo,
+                MessageBoxImage.Question
+            );
+
+            if (buttonClicked == MessageBoxResult.Yes)
+            {
+                RedirectToSearchClientByRFC();
+            }
         }
 
         private void BtnSaveChangesClick(object sender, RoutedEventArgs e)
         {
+            HideFieldsErrors();
 
+            bool isValidClientInformation = ValidateClientPersonalInformation();
+            if(isValidClientInformation)
+            {
+                ShowSaveChangesConfirmationDialog();
+            }
+            else
+            {
+                HighlightInvalidFields();
+                ShowInvalidFieldsAlertDialog();
+            }
+        }
+
+        private void HideFieldsErrors()
+        {
+            TbClientName.Style = (Style)FindResource("TextInput");
+            TbClientLastName.Style = (Style)FindResource("TextInput");
+            TbClientSurname.Style = (Style)FindResource("TextInput");
+            BdrClientBirthDate.BorderBrush = (Brush)FindResource("MediumGray");
+
+            TbAddressStreet.Style = (Style)FindResource("TextInput");
+            TbAddressNeighborhood.Style = (Style)FindResource("TextInput");
+            TbAddressInteriorNumber.Style = (Style)FindResource("TextInput");
+            TbAddressOutdoorNumber.Style = (Style)FindResource("TextInput");
+            TbAddressPostCode.Style = (Style)FindResource("TextInput");
+            TbAddressCity.Style = (Style)FindResource("TextInput");
+            TbAddressMunicipality.Style = (Style)FindResource("TextInput");
+            TbAddressState.Style = (Style)FindResource("TextInput");
+        }
+
+        private bool ValidateClientPersonalInformation()
+        {
+            bool isValidInformation =
+                !string.IsNullOrWhiteSpace(TbClientName.Text)
+                && !string.IsNullOrWhiteSpace(TbClientLastName.Text)
+                && !string.IsNullOrWhiteSpace(TbClientSurname.Text)
+                && DpkClientBirthDate.SelectedDate.HasValue 
+                && DpkClientBirthDate.SelectedDate.Value <= DateTime.Now.AddYears(-18)
+                && !string.IsNullOrWhiteSpace(TbAddressStreet.Text)
+                && !string.IsNullOrWhiteSpace(TbAddressNeighborhood.Text)
+                && !string.IsNullOrWhiteSpace(TbAddressInteriorNumber.Text)
+                && !string.IsNullOrWhiteSpace(TbAddressOutdoorNumber.Text)
+                && !string.IsNullOrWhiteSpace(TbAddressPostCode.Text)
+                && !string.IsNullOrWhiteSpace(TbAddressCity.Text)
+                && !string.IsNullOrWhiteSpace(TbAddressMunicipality.Text)
+                && !string.IsNullOrWhiteSpace(TbAddressState.Text);
+
+            return isValidInformation;
+        }
+
+        private void HighlightInvalidFields()
+        {
+            if (string.IsNullOrWhiteSpace(TbClientName.Text))
+            {
+                TbClientName.Style = (Style)FindResource("TextInputError");
+            }
+
+            if (string.IsNullOrWhiteSpace(TbClientLastName.Text))
+            {
+                TbClientLastName.Style = (Style)FindResource("TextInputError");
+            }
+
+            if (string.IsNullOrWhiteSpace(TbClientSurname.Text))
+            {
+                TbClientSurname.Style = (Style)FindResource("TextInputError");
+            }
+
+            if (!DpkClientBirthDate.SelectedDate.HasValue
+                || DpkClientBirthDate.SelectedDate.Value > DateTime.Now.AddYears(-18))
+            {
+                BdrClientBirthDate.BorderBrush = (Brush)FindResource("Red");
+            }
+
+            if (string.IsNullOrWhiteSpace(TbAddressStreet.Text))
+            {
+                TbAddressStreet.Style = (Style)FindResource("TextInputError");
+            }
+
+            if (string.IsNullOrWhiteSpace(TbAddressNeighborhood.Text))
+            {
+                TbAddressNeighborhood.Style = (Style)FindResource("TextInputError");
+            }
+
+            if (string.IsNullOrWhiteSpace(TbAddressInteriorNumber.Text))
+            {
+                TbAddressInteriorNumber.Style = (Style)FindResource("TextInputError");
+            }
+
+            if (string.IsNullOrWhiteSpace(TbAddressOutdoorNumber.Text))
+            {
+                TbAddressOutdoorNumber.Style = (Style)FindResource("TextInputError");
+            }
+
+            if (string.IsNullOrWhiteSpace(TbAddressPostCode.Text))
+            {
+                TbAddressPostCode.Style = (Style)FindResource("TextInputError");
+            }
+
+            if (string.IsNullOrWhiteSpace(TbAddressCity.Text))
+            {
+                TbAddressCity.Style = (Style)FindResource("TextInputError");
+            }
+
+            if (string.IsNullOrWhiteSpace(TbAddressMunicipality.Text))
+            {
+                TbAddressMunicipality.Style = (Style)FindResource("TextInputError");
+            }
+
+            if (string.IsNullOrWhiteSpace(TbAddressState.Text))
+            {
+                TbAddressState.Style = (Style)FindResource("TextInputError");
+            }
+        }
+        
+        private void ShowInvalidFieldsAlertDialog()
+        {
+            MessageBox.Show(
+                "Verifique que la información ingresada en los campos marcados " +
+                "en rojo sea correcta. Recuerde que el cliente debe ser mayor de edad",
+                "Campos inválidos",
+                MessageBoxButton.OK,
+                MessageBoxImage.Warning
+            );
+        }
+
+        private void ShowSaveChangesConfirmationDialog()
+        {
+            MessageBoxResult buttonClicked = MessageBox.Show(
+                "¿Está seguro que desea guardar los cambios realizados al cliente" 
+                + client.Name + " " + client.LastName,
+                "Confirmación de actualización",
+                MessageBoxButton.YesNo,
+                MessageBoxImage.Question
+            );
+
+
+            if ( buttonClicked == MessageBoxResult.Yes )
+            {
+                UpdateClientPersonalInformation();
+            }
+        }
+
+        private void UpdateClientPersonalInformation()
+        {
+
+        }
+
+        private void RestrictToPlainText(TextBox textBox)
+        {
+            textBox.PreviewTextInput += (sender, e) =>
+            {
+                if (!DataValidator.IsPlainText(new string(e.Text[e.Text.Length - 1], 1)))
+                {
+                    e.Handled = true;
+                }
+            };
+        }
+
+        private void RestrictToNumeric(TextBox textBox)
+        {
+            textBox.PreviewTextInput += (sender, e) =>
+            {
+                if (!char.IsDigit(e.Text, e.Text.Length - 1))
+                {
+                    e.Handled = true;
+                }
+            };
+            textBox.PreviewKeyDown += (sender, e) =>
+            {
+                if (e.Key == Key.Space)
+                {
+                    e.Handled = true;
+                }
+            };
         }
     }
 }
