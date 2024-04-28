5555fb85c90e45007321237ce0e7c02b2ad91bad
DonajiNavarro15 <ada_hack49@hackademy.mx>
2024-04-28 13:26:33 -0600

    feat(ConsultPaytableView): Implements the gui for the paytable

---
+++ SFIClient/Views/ConsultPaytableView.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SFIClient.Views
+{
+    /// <summary>
+    /// Lógica de interacción para ConsultPaytableView.xaml
+    /// </summary>
+    public partial class ConsultPaytableView : Page
+    {
+        private void PageLoaded(object sender, RoutedEventArgs e)
+        {
+        }
+        public ConsultPaytableView()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnRegisterPaymentClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnReturnCreditsListClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
