eaa153eef2453eabedeca68e0ab1486d6a6d5871
rodrigo19112003 <zS21013854@estudiantes.uv.mx>
2024-05-08 19:33:06 -0600

    feat(LiveCharts): implements configuration to create charts

---
+++ SFIClient/Views/OverallCollectionEfficiencyView.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using LiveCharts;
+using LiveCharts.Definitions.Series;
+using LiveCharts.Wpf;
+
+namespace SFIClient.Views
+{
+    /// <summary>
+    /// Lógica de interacción para OverallCollectionEfficiency.xaml
+    /// </summary>
+    public partial class OverallCollectionEfficiencyController : Page
+    {
+        public OverallCollectionEfficiencyController()
+        {
+            InitializeComponent();
+            ShowChart();
+        }
+
+        public void ShowChart()
+        {
+            SeriesCollection pieSeries = new SeriesCollection();
+            // Agregar datos de ejemplo
+            pieSeries.Add(new PieSeries
+            {
+                Title = "Category A",
+                Values = new ChartValues<double> { 10 }
+            });
+
+            pieSeries.Add(new PieSeries
+            {
+                Title = "Category B",
+                Values = new ChartValues<double> { 20 }
+            });
+
+            pieSeries.Add(new PieSeries
+            {
+                Title = "Category C",
+                Values = new ChartValues<double> { 30 }
+            });
+
+            // Asignar la colección de series al PieChart
+            pcChart.Series = pieSeries;
+        }
+    }
+}
