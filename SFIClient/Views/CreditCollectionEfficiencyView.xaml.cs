7ab373536095f5ee2da452eff1488bf2d8578a77
Angel Garcia <adjcg15@gmail.com>
2024-05-25 21:07:54 -0600

    feat(CreditCollectionEfficiencyView): implements credit, client and efficiency information showing

--- SFIClient/Views/CreditCollectionEfficiencyView.xaml.cs
+++ SFIClient/Views/CreditCollectionEfficiencyView.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using SFIClient.SFIServices;
+using SFIClient.Utilities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -42,7 +43,10 @@ namespace SFIClient.Views
             {
                 Credit recoveredCredit = creditsService.GetCreditForCollectionEfficiency(creditInvoice);
                 credit = recoveredCredit;
-                //TODO: show credit information
+
+                ShowCreditInformation();
+                ShowClientInformation();
+                ShowEfficiencyInformation();
             }
             catch (FaultException<ServiceFault> fault)
             {
@@ -59,6 +63,45 @@ namespace SFIClient.Views
                 string errorMessage = "Error de conexión. Ocurrió un error de conexión, por favor compruebe " +
                     "su conexión de Internet e intente de nuevo";
                 ShowErrorRecoveringCreditDialog(errorMessage);
+            }
+        }
+
+        private void ShowCreditInformation()
+        {
+            TbkCreditInvoice.Text = credit.Invoice;
+            TbkCreditAmountApproved.Text = credit.AmountApproved.ToString("C", new System.Globalization.CultureInfo("es-MX"));
+        }
+
+        private void ShowClientInformation()
+        {
+            Client client = credit.Client;
+            TbkClientName.Text = client.Name + " " + client.LastName + " " + client.Surname;
+            TbkClientRFC.Text = client.Rfc;
+        }
+
+        private void ShowEfficiencyInformation()
+        {
+            List<Payment> payments = credit.Payments.ToList();
+            int totalPayments = payments.Count;
+
+            List<Payment> paymentsPlannedToDate = payments
+                .Where(payment => payment.planned_date <= DateTime.Now)
+                .ToList();
+            int totalPaymentPlannedToDate = paymentsPlannedToDate.Count;
+            int totalPaidPayments = paymentsPlannedToDate
+                .Where(payment => payment.reconciliation_date != null)
+                .ToList()
+                .Count;
+
+            TbkTotalPayments.Text = totalPayments.ToString();
+            TbkTotalPaymentPlannedToDate.Text = totalPaymentPlannedToDate.ToString();
+
+            if(totalPaymentPlannedToDate > 0) {
+                double efficiencyPercentage = (totalPaidPayments / totalPaymentPlannedToDate) * 100.0;
+                TbkEfficiency.Text = efficiencyPercentage.ToString("0.0") + "%";
+            } else
+            {
+                TbkEfficiency.Text = "No aplica";
             }
         }
 
